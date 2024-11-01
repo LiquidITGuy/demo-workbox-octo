bf32b5460fe7eae91ab6078f150cc61f376f13ef
Simon Belbeoch <simon.belbeoch@ln1.eu>
2024-11-01 10:22:45 +0100

    ajout du nombre d'appel

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,8 @@
 const SW_VERSION = "1.0.0"
-let numberOfCall = 0
-let quantityOfData = 0
+let DEFAULT_NUMBER_OF_CALL = 0
+let DEFAULT_QUANTITY_OF_DATA = 0
+const CACHE_NAME = 'api-cache';
+const dataCached = new Map()
 self.addEventListener('message', (event) => {
     if (event.data.type === 'GET_VERSION') {
         event.ports[0].postMessage(SW_VERSION);
@@ -21,13 +23,18 @@ self.addEventListener('fetch', async (event) => {
     if (event.request.url.includes('livres')) { // on met en cache uniquement les requêtes à 'livres'
         const cache = await caches.match(event.request);
         if (cache) {
-            console.log("Service Worker: Cache hit");
+            const value = dataCached.get(event.request.url)
+            if(value) {
+                dataCached.set(event.request.url, value + 1)
+            }
+            console.log("Service Worker: Cache hit value", value, '& url', event.request.url);
             return cache;
         }
         try {
             const response = await fetch(event.request);
             await caches.open('api-cache').then(function(cache) {
                 console.log("Service Worker: Cache miss");
+                dataCached.set(event.request.url, 1)
                 cache.put(event.request, response);
             });
             return response;
@@ -35,21 +42,21 @@ self.addEventListener('fetch', async (event) => {
             console.log("Service Worker: Error: ", error);
         }
     }
+    /*
+        const result = fetch(event.request)
+        numberOfCall += 1
 
-    const result = fetch(event.request)
-    numberOfCall += 1
+        result.then(response => {
+            if(response.headers.get("content-length")) {
+                quantityOfData += Number(response.headers.get("content-length"))
+            }else {
+                const encoder = new TextEncoder();
+                const bytes = encoder.encode(response.body);
+                const contentSize = bytes.length;
+                quantityOfData += contentSize;           }
+        })
 
-    result.then(response => {
-        if(response.headers.get("content-length")) {
-            quantityOfData += Number(response.headers.get("content-length"))
-        }else {
-            const encoder = new TextEncoder();
-            const bytes = encoder.encode(response.body);
-            const contentSize = bytes.length;
-            quantityOfData += contentSize;           }
-    })
-
-    console.log("numberOfCall", numberOfCall)
-    console.log("quantityOfData", quantityOfData/1024/1024)
-    event.respondWith(result);
+        console.log("numberOfCall", numberOfCall)
+        console.log("quantityOfData", quantityOfData/1024/1024)
+        event.respondWith(result);*/
 });
